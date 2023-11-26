e9af2d0d3c2d5a145b0ea70222319c77bcb8d0e4
Jan Kolenko <jan.kolenko@externals.adidas.com>
2023-11-26 17:04:50 +0100

    change strong logic

--- src/routes/AdminRoute.tsx
+++ src/routes/AdminRoute.tsx
@@ -10,7 +10,9 @@ export const AdminRoute: React.FC = observer(() => {
         <div className="route-admin">
             <h3>H3 title</h3>
             <p>Paragraph text</p>
-            <p className="bold">Paragraph text bold</p>
+            <p>
+                <strong>Paragraph text bold</strong> and now its not
+            </p>
             <Button onClick={() => (todosStore.toggle = !todosStore.toggle)}>Toggle {`${todosStore.toggle}`}</Button>
             <p>{JSON.stringify(todosStore.todos)}</p>
         </div>
