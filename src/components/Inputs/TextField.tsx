d37da36b1ebcdc3fee14e00a7747be24e58a594b
JernejBezjak <jernej.bezjak@inova.si>
2023-11-29 09:47:09 +0100

    created TextField component

---
+++ src/components/Inputs/TextField.tsx
@@ -0,0 +1,26 @@
+import { observer } from "mobx-react-lite";
+import "../Inputs/TextField.scss"
+
+interface ITextFieldProps {
+    onChange?: (value: string) => void,
+    placeholder?: string
+    value?: string
+    className?: string
+    type?: string
+}
+
+export const TextField: React.FC<ITextFieldProps> = observer(({ onChange, placeholder, value, className, type }) => {
+    return (
+        <input
+            type={type}
+            placeholder={placeholder}
+            onChange={(e) => onChange(e.target.value)}
+            value={value}
+            className={`base-input ${className}`} />
+    )
+})
+
+TextField.defaultProps = {
+    placeholder: "Search",
+    type: "text",
+}
