2456428b18018858edc54e11220d0d7d5c16c94a
JernejBezjak <jernej.bezjak@inova.si>
2023-12-04 09:25:07 +0100

    Added on hover effect for checkmark

--- src/components/Inputs/checkbox/CheckBox.tsx
+++ src/components/Inputs/checkbox/CheckBox.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './CheckBox.scss';
+import "../../typography/Text.scss"
 
 interface ICheckboxProps {
     isChecked?: boolean;
     label?: string;
+    onChange?: boolean;
 }
 
-export const CheckBox: React.FC<ICheckboxProps> = ({ isChecked, label }) => {
+export const CheckBox: React.FC<ICheckboxProps> = ({ isChecked, label, onChange }) => {
 
     return (
-        <label className="container">
+        <label className="container base-text body-m">
             {label}
             <input type="checkbox" checked={isChecked} />
-            <span className="checkmark">
+            <div className="checkmark">
                 <div className='circle'></div>
-            </span>
+            </div>
         </label>
     );
 };
