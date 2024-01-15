eb5d6618fc38504506a38cc6360ef0d755e6f236
JernejBezjak <jernej.bezjak@inova.si>
2024-01-15 10:16:39 +0100

    Added NavLink and style to the side menu

--- src/routes/menus/SideMenu.tsx
+++ src/routes/menus/SideMenu.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite"
 import "./SideMenu.scss"
-import { useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { Text } from "../../components/typography/Text"
 import { AuthService } from "../../services"
 
@@ -9,33 +9,40 @@ interface ISideMenuProps {
 }
 
 export const SideMenu: React.FC<ISideMenuProps> = observer(({ isSideMenuVisible }) => {
-    const navigate = useNavigate()
     const isSideMenuVisibleClass = isSideMenuVisible ? "visible" : "hidden"
 
     return (
         <div className={`side-menu ${isSideMenuVisibleClass}`}>
             <ul className="list-items">
                 <li>
-                    <a onClick={() => navigate("/")}>
-                        <Text variant="body-s">Dashboard</Text>
-                    </a>
+                    <NavLink to={"/"}>
+                        <Text variant="body-s" color="text-secondary" className="text-hover">
+                            Dashboard
+                        </Text>
+                    </NavLink>
                 </li>
                 <li>
-                    <a onClick={() => navigate("/components")}>
-                        <Text variant="body-s">Components</Text>
-                    </a>
+                    <NavLink to={"/components"}>
+                        <Text variant="body-s" color="text-secondary" className="text-hover">
+                            Components
+                        </Text>
+                    </NavLink>
                 </li>
                 {AuthService.isAdmin && (
                     <li>
-                        <a onClick={() => navigate("/example")}>
-                            <Text variant="body-s">Example</Text>
-                        </a>
+                        <NavLink to={"/example"}>
+                            <Text variant="body-s" color="text-secondary" className="text-hover">
+                                Example
+                            </Text>
+                        </NavLink>
                     </li>
                 )}
                 <li>
-                    <a onClick={() => navigate("/profile")}>
-                        <Text variant="body-s">Profile</Text>
-                    </a>
+                    <NavLink to={"/profile"}>
+                        <Text variant="body-s" color="text-secondary" className="text-hover">
+                            Profile
+                        </Text>
+                    </NavLink>
                 </li>
             </ul>
         </div>
